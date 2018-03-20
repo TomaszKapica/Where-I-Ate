5e32ef90c6aca62c19f7b323f7b1ec9c81a23b7a
TomaszKapica <tomaszkapica95@gmail.com>
2018-03-20 17:25:17 +0100

    Added bordering result in profile page, searching profile attributes

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 from django.views.generic import DetailView
 from django.shortcuts import Http404
+from restaurants.models import Restaurant
+from menus.models import Item
+from django.db.models import Q
+
 User = get_user_model()
 
 
@@ -14,3 +18,23 @@ class ProfileDetailView(DetailView):
         if username is None:
             raise Http404
         return get_object_or_404(User, username__iexact=username)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(*args, **kwargs)
+        query = self.request.GET.get('qs')
+        owner_qs = context['restaurantuser']
+        item_qs = Item.objects.filter(owner=owner_qs)
+        rest_qs = Restaurant.objects.filter(owner=owner_qs)
+        if query:
+            rest_qs = rest_qs.filter(
+                Q(name__icontains=query)|
+                Q(location__icontains=query)|
+                Q(category__icontains=query)|
+                Q(name__iexact=query)|
+                Q(location__iexact=query)|
+                Q(category__iexact=query)
+            )
+        if rest_qs.exists() and item_qs.exists():
+            context['search_query'] = rest_qs
+        return context
+
